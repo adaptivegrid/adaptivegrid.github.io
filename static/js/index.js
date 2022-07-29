42e072e62cfb6e5e43d14dd224c6aacb9bb40a79
Brezgunova Irina <110252636+adaptivegrid@users.noreply.github.com>
2022-07-29 23:22:22 +0300

    js2

--- static/js/index.js
+++ static/js/index.js
@@ -2,19 +2,25 @@
 var slideIndex = 1;
 showSlides(slideIndex);
 
+/* Увеличиваем индекс на 1 — показываем следующий слайд*/
 function nextSlide() {
     showSlides(slideIndex += 1);
 }
 
+/* Уменьшает индекс на 1 — показываем предыдущий слайд*/
 function previousSlide() {
     showSlides(slideIndex -= 1);  
 }
 
+/* Устанавливаем текущий слайд */
 function currentSlide(n) {
     showSlides(slideIndex = n);
 }
 
+/* Функция перелистывания */
+
 function showSlides(n) {
+    // let i;
     let slides = document.getElementsByClassName("item");
     
     if (n > slides.length) { 
@@ -23,28 +29,31 @@ function showSlides(n) {
     if (n < 1) {
         slideIndex = slides.length;
     }
+  /* Проходим по каждому слайду в цикле for */
     for (let slide of slides) {
         slide.style.display = "none";
         slides[slideIndex - 1].style.display = "block"; 
     }    
 };
 
-
 function changeItem(id){
     let fullDiv = document.getElementById(id);
     let byId = fullDiv.getAttribute("id");
     document.getElementById(byId).style.margin = "-20px 0 20px 0";
     document.getElementById(byId).style.transition = "0.5s";
-    document.getElementById(byId).style.transition = "0.4s";   
+    document.getElementById(byId).style.transition = "0.4s";
+    
 }
-
+// функция, которая сработает при наведении.
+//она означает - выбрать элемент к Id у которого надо что-то изменить.
+// когда в скобки где написано one-two добавите Id своего элемента
 function rechangeItem(id) {
     let fullDiv = document.getElementById(id);
     let byId = fullDiv.getAttribute("id");
     document.getElementById(byId).style.margin = "0 0 0 0";
     document.getElementById(byId).style.transition = "0.5s";
     document.getElementById(byId).style.transitionDelay = "0.3s";
-}
+}// тут всё также. но наобарот. протсес происходящий про отводе курсора.
 
 
 function onEntry(entry) {
